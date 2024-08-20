6c29b4cff99c44dccabbb755801646bd24769e29
muraddagha <muradha@code.edu.az>
2024-08-20 22:52:43 +0400

    error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router";
-import FilterContextProvider from "./store/filter-context";
 import { Provider } from "react-redux";
 import store from "./store/store";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
         <Provider store={store}>
-          <FilterContextProvider>
-            <RouterProvider router={Router} />
-          </FilterContextProvider>
+          <RouterProvider router={Router} />
         </Provider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { formatDate } from "./utils";
-
+import { toast } from "react-toastify";
+import { API_SOURCES } from "../constants";
 export const getData = async (path, page, pageSize) => {
   try {
     const response = await axios.get(path, {
@@ -21,6 +22,8 @@ export const getData = async (path, page, pageSize) => {
     };
   } catch (error) {
     console.error("Error fetching articles:", error);
+    console.log(path);
+    toast.error(API_SOURCES.NEWS_API.URL + error.message);
     return {
       data: [],
       nextPage: page,
@@ -36,6 +39,8 @@ export const getGuardianData = async (path, page, pageSize) => {
     return { data: data.results, nextPage: page + 1, totalResults: data.total };
   } catch (error) {
     console.error("Error fetching articles:", error);
+    toast.error(API_SOURCES.GUARDIAN_API.URL + error.message);
+
     return {
       data: [],
       nextPage: page,
@@ -50,6 +55,8 @@ export const getNYTData = async (path, page, pageSize) => {
     const data = result.data.response;
     return { data: data.docs, nextPage: page + 1, totalResults: data.meta.hits };
   } catch (error) {
+    console.log(path);
+    toast.error(API_SOURCES.NYTIMES.URL + error.message);
     console.error("Error fetching articles:", error);
     return { data: [], nextPage: page, totalResults: 0 };
   }
